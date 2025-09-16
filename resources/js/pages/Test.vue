35fbf745c5f18d34780b8ebd859df328e5bbdd41
Saqaf <saqafaliabdo716@gmail.com>
2025-09-17 02:41:41 +0300

    firs commit

---
+++ resources/js/pages/Test.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { test } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Test',
+        href: test().url,
+    },
+];
+</script>
+
+<template>
+    <Head title="Test" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <form action="">
+            <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+
+
+            </div>
+        </form>
+    </AppLayout>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render(component: 'Dashboard');
+
+
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+    Route::get('test', function () {
+    return Inertia::render('Test');
+})->name('test');
+
+
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
