12804d0f498d9189b49357b2c63b1ef224c8b4d6
Saqaf <saqafaliabdo716@gmail.com>
2025-09-23 02:16:32 +0300

    add: trash actions restore and  final delete

---
+++ resources/js/components/TrashedAcrion.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import Actions from './Actions.vue';
+import DropdownMenuItem from './ui/dropdown-menu/DropdownMenuItem.vue';
+const props = defineProps(['id', 'restoreRoute', 'deleteRoute']);
+const emit = defineEmits(['deleteSusses', 'restoreSusses']);
+const isAlertOpen = ref(false);
+
+const openAlert = () => {
+    isAlertOpen.value = true;
+};
+
+const closeAlert = () => {
+    isAlertOpen.value = false;
+};
+const form = useForm({});
+const deleteItem = () => {
+    form.delete(props.deleteRoute, {
+        onSuccess: () => {
+            emit('deleteSusses', props.id);
+        },
+    });
+
+    closeAlert();
+};
+
+const restoreItem = () => {
+    form.post(props.restoreRoute, {
+        onSuccess: () => {
+            emit('restoreSusses', props.id);
+        },
+    });
+};
+</script>
+
+<template>
+    <Actions>
+        <DropdownMenuItem @select="restoreItem"> Restore </DropdownMenuItem>
+
+        <DropdownMenuItem @select="openAlert"> Delete </DropdownMenuItem>
+    </Actions>
+
+    <AlertDialog :open="isAlertOpen" @update:open="isAlertOpen = $event">
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription> This action cannot be undone. </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel @click="closeAlert">Cancel</AlertDialogCancel>
+                <AlertDialogAction @click="deleteItem">Delete</AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+</template>
