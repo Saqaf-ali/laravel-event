ef54b79627bd9a9918d3c00ad6e9c6a1a7cbaeb8
Saqaf <saqafaliabdo716@gmail.com>
2025-09-28 23:19:22 +0300

    feat: add ticket system
    This commit introduces a new ticket system.
    
    - Adds `tickets` table with `title`, `description`, `status`, `priority`, and `user_id` columns.
    - Implements basic CRUD operations for tickets.
    - Includes routes and controllers for managing tickets.

--- database/migrations/2025_09_28_201048_create_tickets_table.php
+++ database/migrations/2025_09_28_201048_create_tickets_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Event;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('type');
+            $table->decimal('price', 8, 2);
+            $table->integer('quantity');
+            $table->foreignIdFor(Event::class)->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
