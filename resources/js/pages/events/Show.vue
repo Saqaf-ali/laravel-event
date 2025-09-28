19b0217305490df7885f6b40953938d5c422a662
Saqaf <saqafaliabdo716@gmail.com>
2025-09-28 03:20:29 +0300

    add:style of element

--- resources/js/pages/events/Show.vue
+++ resources/js/pages/events/Show.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import AppContent from '@/components/AppContent.vue';
+import Heading from '@/components/Heading.vue';
+import HeadingSmall from '@/components/HeadingSmall.vue';
 import CarouselImages from '@/components/Navigation/CarouselImages.vue';
+import { CardContent, CardHeader } from '@/components/ui/card';
 import Card from '@/components/ui/card/Card.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
@@ -35,17 +38,28 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <AppContent>
-            <Card class="mx-auto max-w-4xl p-4">
+            <Card class="mx-auto max-w-4xl border-none bg-card p-4">
                 <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-                    <div>
-                        <h2 class="mb-2 text-2xl font-bold">{{ props.event.title }}</h2>
-                        <p class="mb-4 text-gray-600">{{ props.event.description }}</p>
-                        <div class="mb-2"><span class="font-semibold">Location:</span> {{ props.event.location }}</div>
-                        <div class="mb-2">
-                            <span class="font-semibold">Start Date:</span> {{ new Date(props.event.start_date).toLocaleDateString() }}
+                    <CardHeader class="">
+                        <Heading class="text-primary" :title="props.event.title" :description="props.event.description" />
+                    </CardHeader>
+                    <CardContent>
+                        <div class="flex flex-wrap space-y-4">
+                            <AppContent class="flex-2/2">
+                                <HeadingSmall title="Location" :description="props.event.location" />
+                            </AppContent>
+                            <AppContent class="">
+                                <HeadingSmall title="Start Date" :description="new Date(props.event.start_date).toLocaleDateString()" />
+                            </AppContent>
+                            <AppContent>
+                                <HeadingSmall title="End Date" :description="new Date(props.event.end_date).toLocaleDateString()" />
+                            </AppContent>
+                            <AppContent>
+                                <HeadingSmall title="Created At" :description="new Date(props.event.created_at).toLocaleDateString()" />
+                            </AppContent>
                         </div>
-                        <div class="mb-2"><span class="font-semibold">End Date:</span> {{ new Date(props.event.end_date).toLocaleDateString() }}</div>
-                    </div>
+                    </CardContent>
+
                     <div v-if="props.event.event_images && props.event.event_images.length > 0">
                         <CarouselImages :images="props.event.event_images" />
                     </div>
