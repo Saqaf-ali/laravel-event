b44c8f485dadf22406b327665b450fcffb884437
Saqaf <saqafaliabdo716@gmail.com>
2025-10-05 03:01:55 +0300

    feat: add hero page

---
+++ resources/js/components/Web/HeroNormalPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface HeroNormalPageProps {
+    title: string;
+    description: string;
+}
+
+const props = defineProps<HeroNormalPageProps>();
+</script>
+<template>
+    <section class="relative bg-gradient-to-r from-primary to-accent py-20 text-white">
+        <div class="container mx-auto px-4 text-center">
+            <h1 class="mb-4 text-5xl font-bold text-primary-foreground">{{ title }}</h1>
+            <p class="text-xl text-primary-foreground/90">{{ description }}</p>
+        </div>
+    </section>
+</template>
--- resources/js/pages/Web/events/Index.vue
+++ resources/js/pages/Web/events/Index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Heading from '@/components/Heading.vue';
+import HeroNormalPage from '@/components/Web/HeroNormalPage.vue';
 import SliderCard from '@/components/Web/SliderCard.vue';
 import AppLayout from '@/layouts/web/AppLayout.vue';
 import type { Event } from '@/types';
@@ -11,9 +13,14 @@ console.log(props.events);
 
 <template>
     <AppLayout>
-        <section class="bg-accent/10">
-            <div class="container mx-auto grid grid-cols-1 gap-8 py-10 md:grid-cols-2 lg:grid-cols-3">
-                <SliderCard v-for="event in events" :key="event.id" :event="event" class="h-120" />
+        <HeroNormalPage title="Our Events" description="Explore a wide variety of events tailored to your interests." />
+
+        <section class="bg-accent/10 py-16">
+            <div class="container m-auto">
+                <Heading title="Upcoming Events" description="Stay tuned for our latest events" class="mb-10 text-center" />
+                <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                    <SliderCard v-for="event in events" :key="event.id" :event="event" class="h-120" />
+                </div>
             </div>
         </section>
     </AppLayout>
