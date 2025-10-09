34463e07fd466b8c23b0445128de431c251c4153
Saqaf <saqafaliabdo716@gmail.com>
2025-10-09 03:42:25 +0300

    add : test image path

--- resources/js/components/Navigation/CarouselImages.vue
+++ resources/js/components/Navigation/CarouselImages.vue
@@ -9,12 +9,17 @@ interface CarouselImagesProps {
 const props = withDefaults(defineProps<CarouselImagesProps>(), {
     class: 'h-full',
 });
+console.log('fffffffff',props.images);
+console.log('----------------------------------------------------------------------dddddddddddddd');
+
+
 </script>
 
 <template>
     <Carousel>
         <CarouselContent>
             <CarouselItem v-for="(image, index) in props.images" :key="index">
+
                 <div class="h-full cursor-grab p-1">
                     <img :src="image.image_url" alt="Event Image" :class="`inset-0 w-full rounded-lg object-cover shadow-md ${props.class}`" />
                 </div>
