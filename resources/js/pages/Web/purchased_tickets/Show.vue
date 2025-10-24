0dd3556dbf93797c97fbefb5cb082e87cace8270
Saqaf <saqafaliabdo716@gmail.com>
2025-10-25 01:33:24 +0300

    feat: add item component

--- resources/js/pages/Web/purchased_tickets/Show.vue
+++ resources/js/pages/Web/purchased_tickets/Show.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Heading from '@/components/Heading.vue';
+import { Item, ItemContent, ItemDescription, ItemTitle } from '@/components/ui/item';
 import AppLayout from '@/layouts/web/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 
@@ -10,6 +11,7 @@ const { purchasedTicket } = defineProps<{
             ticket_code: string;
             qr_code: string;
             order_item: {
+                id: number;
                 ticket: {
                     type: string;
                     price: number;
@@ -23,10 +25,6 @@ const { purchasedTicket } = defineProps<{
         };
     };
 }>();
-
-// console.log('pu22222222',);
-
-console.log('purchasedTicket111111111', purchasedTicket);
 </script>
 <template>
     <Head title="purchased Tickets" />
@@ -34,8 +32,26 @@ console.log('purchasedTicket111111111', purchasedTicket);
         <section class="py-16">
             <div class="container mx-auto p-4 md:max-w-1/2">
                 <div class="mb-8 text-center">
-                    <Heading title="Purchased Tickets" description="Review your purchased tickets and event details." />
+                    <Heading title="Purchased Ticket" />
                 </div>
+                <Item>
+                    <ItemContent>
+                        <ItemTitle>Ticket Code: {{ purchasedTicket.data.ticket_code }}</ItemTitle>
+                        <ItemDescription>Event: {{ purchasedTicket.data.order_item.ticket.event.title }}</ItemDescription>
+                        <ItemDescription>Ticket Type: {{ purchasedTicket.data.order_item.ticket.type }}</ItemDescription>
+                        <ItemDescription>Price: ${{ purchasedTicket.data.order_item.ticket.price }}</ItemDescription>
+                        <ItemDescription>
+                            Event Date: {{ purchasedTicket.data.order_item.ticket.event.start_date }} -
+                            {{ purchasedTicket.data.order_item.ticket.event.end_date }}
+                        </ItemDescription>
+                        <div class="mt-4">
+                            <img :src="purchasedTicket.data.qr_code" alt="QR Code" class="mx-auto h-48 w-48" />
+                        </div>
+                    </ItemContent>
+                    <!-- <ItemActions>
+                        <Button @click="window.print()">Print Ticket</Button>
+                    </ItemActions> -->
+                </Item>
             </div>
         </section>
     </AppLayout>
