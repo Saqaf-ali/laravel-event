2826c650a7f5b0218b1a9691644154c701ed4d8c
Saqaf <saqafaliabdo716@gmail.com>
2025-10-24 23:50:06 +0300

    fix: add data in object and display data

--- resources/js/pages/Web/purchased_tickets/Show.vue
+++ resources/js/pages/Web/purchased_tickets/Show.vue
@@ -3,14 +3,33 @@ import Heading from '@/components/Heading.vue';
 import AppLayout from '@/layouts/web/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 
-const props = defineProps(['purchasedTicket']);
+const { purchasedTicket } = defineProps<{
+    purchasedTicket: {
+        data: {
+            id: number;
+            ticket_code: string;
+            qr_code: string;
+            order_item: {
+                ticket: {
+                    type: string;
+                    price: number;
+                    event: {
+                        title: string;
+                        start_date: string;
+                        end_date: string;
+                    };
+                };
+            };
+        };
+    };
+}>();
 
 // console.log('pu22222222',);
 
-console.log('purchasedTicket111111111', props.purchasedTicket);
+console.log('purchasedTicket111111111', purchasedTicket);
 </script>
 <template>
-    <Head title="Checkout" />
+    <Head title="purchased Tickets" />
     <AppLayout>
         <section class="py-16">
             <div class="container mx-auto p-4 md:max-w-1/2">
